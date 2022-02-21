3c73e7ca259a695ab5ff7745d26e4b3c18fafb7a
Cesarpipe31 <98058249+Cesarpipe31@users.noreply.github.com>
2022-02-21 15:58:10 -0500

    other

---
+++ 04-JS-III/homework/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+function incrementarPorUno(array) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Aumenta cada entero por 1
+  // y devuelve el array
+  // Tu código:
+  return array[]
+}
